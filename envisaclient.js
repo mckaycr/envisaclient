25f9cd5dedb3591dfddbd414c305cd68e75b848a
Chris McKay <mckaycr@gmail.com>
2015-04-14 20:54:54 -0400

    Update envisaclient.js
    Made some changes based on recommendations from Node Net Documentation.  Also added the new object.js integration

--- envisaclient.js
+++ envisaclient.js
@@ -1,26 +1,24 @@
+var sys_response = require('./objects.js');
 var net = require('net')
 	,syslog = require('syslogudp')
 	,logger = syslog.createClient(514,'10.0.0.40');
-var client = new net.Socket();
 var strReady = "%00,01,1C08,08,00, MCKAY'S SYSTEM   Ready to Arm  $"
-
+var client = net.createConnection(4025, '10.0.0.205');
 client.setEncoding('utf8');
-client.connect(4025, '10.0.0.205', function() {
-	logger.log('Initiating Envisalink client connection...', syslog.LOG_INFO);
-	client.write('user');
-});
-
 client.on('data', function(data) {
-	var arrLines = data.split('\n')
+  var arrLines = data.split('\n')
 	for(i=0;i<arrLines.length-1;i++){
-		if(arrLines[i]!=strReady){
-			logger.log(arrLines[i],syslog.LOG_INFO);
-			console.log(arrLines[i]);
-		};
+		if(arrLines[i]!=null){
+			//console.log(arrLines[i].trim().length)
+			console.log(sys_response(arrLines[i].trim()).type)
+		}
+		//logger.log(arrLines[i],syslog.LOG_INFO);
+		
 	};
-});
- 
-client.on('close', function() {
-	console.log('Connection closed');
+}).on('connect', function() {
+  // Immediately after connection, send login Password
+  client.write('MoeBas')
+}).on('end', function() {
+  console.log('DONE');
 });
 
