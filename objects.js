6b7d7e6fce79f2771d372711f25670bb16ad7934
Chris McKay <mckaycr@gmail.com>
2015-04-19 15:02:00 -0400

    Updated objects.js and test.js
    Added comments
    Added  end of line terminators
    Corrected indentation in the iconLED function
    Added zoneFaults function to handle the state changes message.
    
    Updated to test the arrZones function.

--- objects.js
+++ objects.js
@@ -1,6 +1,12 @@
 
 module.exports = function(message){
+	//This module accepts a string from the Envsialink and converts it to an object
+	//Eamples of expected responses:
+	//	%01,5C08,08,00,two line 32 byte ASCII String of test$
+	//	%01,0100000000000080$
+	//hex2bin will help convert some of this string into binary
     var bin = require('./hex2bin');
+    //The very first part of the string will contain of the properties within the evl_ResponseType
     var evl_ResponseType = {
         'Login:' : {name:'Login Prompt',description:'Sent During Session Login Only.', handler:'login'},
         'OK' : {name:'Login Success', description: 'Send During Session Login Only, successful login', handler: 'login_success'},
@@ -16,7 +22,8 @@ module.exports = function(message){
         '^02' : {type: 'envisalink', name:'Dump Zone Timers', description: 'This command contains the raw zone timers used inside the Envisalink. The dump is a 256 character packed HEX string representing 64 UINT16 (little endian) zone timers. Zone timers count down from 0xFFFF (zone is open) to 0x0000 (zone is closed too long ago to remember). Each tick of the zone time is actually 5 seconds so a zone timer of 0xFFFE means 5 seconds ago. Remember, the zone timers are LITTLE ENDIAN so the above example would be transmitted as FEFF.',handler: 'command_response'},
         '^03' : {type: 'envisalink', name:'Keypress to Specific Partition', description: 'This will send a keystroke to the panel from an arbitrary partition. Use this if you dont want to change the TPI default partition.' ,handler: 'command_response'},
         '^0C' : {type: 'envisalink', name:'Response for Invalid Command', description: 'This response is returned when an invalid command number is passed to Envisalink', handler: 'command_response'}
-    }
+    };
+    //If the evl_ResponseType isa Virtal Keypad Updated then this part will be the third element within the string
     var evl_Partition_Status_Code = {
         '00' : {name:'NOT_USED', description:'Partition is not used or doesn\'t exist'},
         '01' : {name:'READY', description:'Ready', pluginhandler: 'disarmed'},
@@ -28,7 +35,8 @@ module.exports = function(message){
         '07' : {name:'EXIT_ENTRY_DELAY', description:'Entry or Exit Delay'},
         '08' : {name:'IN_ALARM', description:'Partition is in Alarm', pluginhandler: 'alarmTriggered'},
         '09' : {name:'ALARM_IN_MEMORY', description:'Alarm Has Occurred (Alarm in Memory)', pluginhandler: 'alarmCleared'},
-    }
+    };
+    //This is the fourth part of the Virtual Keypad Update
     var BEEP_field ={
         '00':'OFF',
         '01':'Beep Once',
@@ -36,42 +44,66 @@ module.exports = function(message){
         '03':'Beep Thrice',
         '04':'Coninuous Fast Beep (trouble/urgency)',
         '05':'Continuous Slow Beep (exit delay)'
-    }
-    //Required in order to interpret the bitfield
+    };
+    //This is the second part of the Virtual Keypad Update, and its a little more complicated because it comes in the form of a hex
+    //which then needs to be converted to bitfield and read.  I think there is a better way of doing this.
     var iconLED = (function(bitfield){
         var status = {'ARMED STAY':bitfield[0],'LOW BATTERY':bitfield[1],'FIRE':bitfield[2],'READY':bitfield[3],
                       'Not Used1':bitfield[4],'Not Used2':bitfield[5],'CHECK ICON – SYSTEM TROUBLE':bitfield[6],
                       'ALARM (FIRE ZONE)':bitfield[7],'ARMED (ZERO ENTRY DELAY)':bitfield[8],'Not Used3':bitfield[9],
                       'CHIME':bitfield[10],'BYPASS (Zones are bypassed)':bitfield[11],'AC PRESENT':bitfield[12],
                       'ARMED AWAY':bitfield[13],'ALARM IN MEMORY':bitfield[14],'ALARM (System is in Alarm)':bitfield[15]
-                    }
-        var tmp = ''
-    for (var key in status) {
-      if (status.hasOwnProperty(key) && status[key]==1) {
-        if(key.indexOf("Not Used")==-1){
-            tmp = tmp + ',' + key;
-            }
-      }
-    }
-    if(tmp[0]==','){tmp= tmp.substring(1);}
+                    };
+	    var tmp = '';
+	    for (var key in status) {
+	      if (status.hasOwnProperty(key) && status[key]==1) {
+	        if(key.indexOf("Not Used")==-1){
+	            tmp = tmp + ',' + key;
+	        };
+	      };
+	    };
+	    if(tmp[0]==','){tmp= tmp.substring(1);}
 
-    return tmp
+	    return tmp;
     })
-  var arrResponse = message.split(',')
+
+	var zoneFaults = (function(str,num_char){
+		var tmpdata = ''
+		for(i=0;i<=16;i=i+num_char){
+			tmpdata = tmpdata + reverseString(bin(str.substr(i,num_char)).result)
+		}
+	 	return tmpdata.split('').map(Number).map(Boolean)
+	})
+	//This function takes the 8byte Hex converts it to a 64 bit bitfield. If true returns the index indicating which zones are at fault.
+	//While the string is little-endian, the individual 8 bytes are normal big-endian, MSbit on the left, so there is a swap that takes place ever num_char characters
+	function arrZones(str,num_char){
+		var tmpdata = ''
+		for(var i=0;i<=16;i=i+num_char){
+			tmpdata = tmpdata + bin(str.substr(i,num_char)).result.split('').reverse().join('')
+		}
+		tmpdata.split('').map(Number).map(Boolean)
+		var arrZones =[];
+		for(i=0;i<=tmpdata.length;i++){
+			if(tmpdata.substr(i,1)==1){ arrZones.push(i+1)}
+		}
+		return arrZones;
+	}
+    //This is the final part which assigns the the converted elements into the property
+	var arrResponse = message.split(',')
     if(arrResponse.length>2){
-    objResponse = {
-        type : evl_ResponseType[arrResponse[0]].name,
-        partition : evl_Partition_Status_Code[arrResponse[1]].description,
-        icons : iconLED(bin(arrResponse[2]).result),
-        numeric : arrResponse[3],
-        beeps : BEEP_field[arrResponse[4]],
-        msg  : arrResponse[5].replace('$','').trim()}
-    } else {
+	    objResponse = {
+	        type : evl_ResponseType[arrResponse[0]].name,
+	        partition : evl_Partition_Status_Code[arrResponse[1]].description,
+	        icons : iconLED(bin(arrResponse[2]).result),
+	        numeric : arrResponse[3],
+	        beeps : BEEP_field[arrResponse[4]],
+	        msg  : arrResponse[5].replace('$','').trim()};
+    	} 
+    else {
         objResponse = {
             type : evl_ResponseType[arrResponse[0]].name
-            }
-        }
-
+        };
+    }
     return objResponse;                
 }
 
--- test.js
+++ test.js
@@ -1,31 +1,20 @@
 var bin = require('./hex2bin.js')
-	,sys_response = require('./objects.js')
-	//strEX is the expected response from Envisalink
-	,strEX = "%00,01,1C08,08,00, MCKAY'S SYSTEM   Ready to Arm  $"
-	,data = endianSwap('1000000000000000',2)
-	,Bitfield = require('bitfield')
-	,field = new Bitfield(data.length);
+	//sys_response = require('./objects.js');
 
-//Testing objects.js output
-console.log(sys_response(strEX).type)
+console.log(testZones = arrZones('0100000000000080',2))
 
 
 //Test decoding of the zone/partition state change response 
 
-for(i=0;i<=data.length;i++){
-	field.set(i,data[i])
-	if(field.get(i)==true)console.log('Zone '+(i+1)+' is open/faulted')
-}
-
-// 
-function endianSwap(str,num_char){
-	tmpdata = ''
-	for(i=0;i<=16;i=i+num_char){
-		tmpdata = tmpdata + reverseString(bin(str.substr(i,num_char)).result)
+function arrZones(str,num_char){
+	var tmpdata = ''
+	for(var i=0;i<=16;i=i+num_char){
+		tmpdata = tmpdata + bin(str.substr(i,num_char)).result.split('').reverse().join('')
 	}
- 	return tmpdata.split('').map(Number).map(Boolean)
-}
-// Reverse the characters in a string
-function reverseString(str) {
-	return str.split('').reverse().join('');
+	tmpdata.split('').map(Number).map(Boolean)
+	var arrZones =[];
+	for(i=0;i<=tmpdata.length;i++){
+		if(tmpdata.substr(i,1)==1){ arrZones.push(i+1)}
+	}
+	return arrZones;
 }
