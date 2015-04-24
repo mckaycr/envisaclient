556db35106d6d3a16ad752f0dbea65f9d826b2ad
Chris McKay <mckaycr@gmail.com>
2015-04-24 10:25:07 -0400

    Updates to test file and envisaclient
    Just to be clear, the test file is how I test my scripts without having
    to connect to the envisalink module.  These changes aren't really that
    important.
    
    Now that the objects file is ready for production, I'm updating
    envisaclient.js.  Added a lot of comments to help understand what I'm
    doing as well as minor changes so that this script works with the
    objects module properly

--- test.js
+++ test.js
@@ -1,19 +1,6 @@
 var bin = require('./hex2bin.js')
 	,sys_response = require('./objects.js')
+	,strEX = "%02,0100010000000003$";
 
 
 console.log(sys_response(strEX))
-
-	function zoneFaults(str,num_char){
-		var tmpdata = ''
-		for(var i=0;i<=16;i=i+num_char){
-			console.log(bin(str.substr(i,num_char)).result)
-			//tmpdata = tmpdata + bin(str.substr(i,num_char)).result.split('').reverse().join('')
-		}
-		tmpdata.split('').map(Number).map(Boolean)
-		var arrZones =[];
-		for(i=0;i<=tmpdata.length;i++){
-			if(tmpdata.substr(i,1)==1){ arrZones.push(i+1)}
-		}
-		return arrZones;
-	}
