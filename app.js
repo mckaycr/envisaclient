4caeaad7da6e4d0c0216e6ad58511846047b7d0b
Chris McKay <mckaycr@gmail.com>
2015-04-29 14:07:53 -0400

    Update app.js
    Now the TCP client can forward envisalink messages to the webserver

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var app = require('http').createServer(handler)
 	//net is required to make the TCP connection to the Envisalink
 	,net = require('net')
 	//These two lines are used only if you want to record everything to a syslog server.
-	,syslog = require('syslogudp')
-	,logger = syslog.createClient(514,'10.0.0.40')
+	//,syslog = require('syslogudp')
+	//,logger = syslog.createClient(514,'10.0.0.40')
 	//	strReady indicates the normal state of the system
 	//	I don't want to fill my syslog with this, so if this then don't log
 	,strReady = "%11,01,1C08,08,00"
 	//This is the connection to Envisalink
-	,client = net.createConnection(4025, '10.0.0.205');
+	,client = net.createConnection(4025, '127.0.0.1');
 
 //have no idea what this does besides maybe error handling in case the index file doesn't exist
 function handler (req, res) {
@@ -31,17 +31,20 @@ function handler (req, res) {
 //handle comms between the web server and the web clients
 io.on('connection', function (socket) {
 	//once a webclient has connected, send this object to their browser.
-  	socket.emit('news', 'Server established connection to Client');
+  	socket.emit('welcome', 'Server established connection to Client');
   	//their browser will respond immediately on connection with this
-  	socket.on('my other event', function (data) {
-  	//That response so sent to console
-    console.log(data);
-  }).on('data',function(data){
-  	console.log('This happens when a button is clicked')
+  	socket.on('thank you', function (data) {
+	  	//That response so sent to console
+	    console.log(data);
+	})
+  	socket.on('cmd',function (data){
+  		socket.emit('data',data)
+  		console.log(data)
 	})
 });
+
 //Starts web server
-app.listen(80)
+app.listen(8181)
 
 //handles comms to and from the envisaclient
 client.setEncoding('utf8');
@@ -54,20 +57,25 @@ client.on('data', function (resp) {
 		// 	Check for empty responses, and anything that matches the system ready response
 		//	We don't want to fill the log with those
 		if(arrLines[i]!=null && arrLines[i].search(strReady)==-1){
-			console.log(arrLines[i])
-			logger.log(sys_response(arrLines[i].trim()).raw,syslog.LOG_INFO);
-			//socket.emit('news',sys_response(arrLines[i].trim()).raw)
+			//console.log(arrLines[i])
+			io.sockets.emit('cmd',sys_response(arrLines[i].trim()))
 		}
 	};
 }).on('connect', function() {
 	// Immediately after connection, send login Password
-	logger.log('Authentication process initiated',syslog.LOG_INFO);
+	//logger.log('Authentication process initiated',syslog.LOG_INFO);
 	//send password to EnvisaLink
 	client.write('password')
 }).on('error', function() {
 	// The server is already connected to something (or thinks it is)
-	logger.log('Oops... Something is wrong. Restart you Envisalink module.',syslog.LOG_INFO);
+	//logger.log('Oops... Something is wrong. Restart you Envisalink module.',syslog.LOG_INFO);
 }).on('end', function() {
 	//	Connect was ended
-	logger.log('Connection to Server Terminated',syslog.LOG_INFO);
+	//logger.log('Connection to Server Terminated',syslog.LOG_INFO);
+});
+
+process.on('SIGINT', function() {
+  io.close();
+  client.end()
+  process.exit();
 });
