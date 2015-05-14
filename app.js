6db1712199944b3bce64b8726286a4ef0ac6b740
mckaycr <mckaycr@gmail.com>
2015-05-14 08:45:43 -0400

    Major changes here.  I implemented express because all my research on modules I would like to include in future releases all say they work with express.  I seemed essential that I conform to the masses

--- app.js
+++ app.js
@@ -1,98 +1,60 @@
-//http, socket.io, and fs are required for the web server part of this app.
-var app = require('http').createServer(handler)
-	,io = require('socket.io')(app)
-	,fs = require('fs')
-	//objects is a custom module I use to translate the responses from Evnsialink
-	,sys_response = require('./objects.js')
-	//net is required to make the TCP connection to the Envisalink
-	,net = require('net')
-	//These two lines are used only if you want to record everything to a syslog server.
-	,syslog = require('syslogudp')
-	,logger = syslog.createClient(514,'10.0.0.40')
-	//	strReady indicates the normal state of the system
-	//	I don't want to fill my syslog with this, so if this then don't log
-	,strReady = "%00,01,1C08,08,00"
-	//This is the connection to Envisalink
-	,client = net.createConnection(4025, '10.0.0.205')
-	,url = require('url')
-	,path = require('path');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var mimeTypes = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
-    "png": "image/png",
-    "js": "text/javascript",
-    "css": "text/css"};
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
+var app = express();
 
-function handler (req, res) {
-	if(req.url=="/"){strpath = '/index.html'}else{strpath=req.url}
-  fs.readFile(__dirname + strpath,
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200,{'Content-Type':mimeTypes[req.url.split('.')[1]]});
-    res.end(data);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
   });
 }
 
-//handle comms between the web server and the web clients
-io.on('connection', function (socket) {
-	//once a webclient has connected, send this object to their browser.
-  	socket.emit('welcome', 'Server established connection to Client');
-  	//their browser will respond immediately on connection with this
-  	socket.on('thank you', function (data) {
-	  	//That response so sent to console
-	    console.log(data);
-	})
-  	socket.on('rxcmd',function (data){
-  		socket.emit('data',data)
-  		console.log(data)
-	})
-	socket.on('txcmd',function (data){
-		client.write(data)
-  		console.log(data)
-	})
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-//Starts web server
-app.listen(8181)
 
-//handles comms to and from the envisaclient
-client.setEncoding('utf8');
-client.on('data', function (resp) {
-	//	Login was successful, the server will now start talking to the client
-	//	Envisalink seems to send multiple responses either really fast or as one response.
-	//  This splits each line and processes them separate
-	var arrLines = resp.split('\n')
-	for(i=0;i<arrLines.length-1;i++){
-		// 	Check for empty responses, and anything that matches the system ready response
-		//	We don't want to fill the log with those
-		if(arrLines[i]!=null && arrLines[i].search(strReady)==-1){
-			//console.log(sys_response(arrLines[i].trim()))
-			io.sockets.emit('rxcmd',sys_response(arrLines[i].trim()))
-			logger.log(sys_response(arrLines[i].trim()).raw,syslog.LOG_INFO);
-		}
-	};
-}).on('connect', function() {
-	// Immediately after connection, send login Password
-	logger.log('Authentication process initiated',syslog.LOG_INFO);
-	//send password to EnvisaLink
-	console.log('Webserver connected to Envisalink')
-	client.write('password')
-}).on('error', function() {
-	// The server is already connected to something (or thinks it is)
-	logger.log('Oops... Something is wrong. Restart you Envisalink module.',syslog.LOG_INFO);
-}).on('end', function() {
-	//	Connect was ended
-	logger.log('Connection to Server Terminated',syslog.LOG_INFO);
-});
-
-process.on('SIGINT', function() {
-  io.close();
-  client.end()
-  process.exit();
-});
+module.exports = app;
